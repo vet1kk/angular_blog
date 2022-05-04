fb6db88b058c5a931ab688100d3b083916caece9
vet1kk <vet1kk@gmail.com>
2022-05-04 14:36:20 +0300

    form add

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+const routes: Routes = [
+  {
+    path: '', component: AdminLayoutComponent, children: [
+      {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
+      {path: 'login', component: LoginPageComponent},
+      {path: 'dashboard', component: DashboardPageComponent},
+      {path: 'create', component: CreatePageComponent},
+      {path: 'post/:id/edit', component: EditPageComponent}
+    ]
+  }
+]
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: [
+    AdminLayoutComponent,
+    LoginPageComponent,
+    DashboardPageComponent,
+    CreatePageComponent,
+    EditPageComponent
+  ]
+})
+
+
+export class AdminModule {
+
+}
